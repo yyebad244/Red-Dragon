06ee8ad8cf8375c45fc9cbfd67437264d8d9eee2
yyebad244 <108775601+yyebad244@users.noreply.github.com>
2022-07-06 07:44:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //════════════(00)════════════════\\
 //
-//          RED DRAGON v6.0
+//          sOFTbOTS v6.0
 //
 //════════════(00)════════════════\\
 
@@ -19,31 +19,31 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is sOFTbOT🔥  
+My owner is anonymous killer😎 Who is the founder of SOFTBOTS group 🔥` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94725614416'] //Owner number in aive msg
+global.premium = ['+94725614416'] //Owner Number info
+global.ownernomer = '+94725614416' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '😎WTF.anonymous' //Owner Name
+global.botname = ' ☬sOFT༒bOTS࿐' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = '🤭Twitter📍' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://twitter.com/SahanRav1' //Change Your Button's URL
 
-global.region = 'India, South India, Kerala' //Country Region
+global.region = 'I am everywhere' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬sOFT༒bOTS࿐' //Bot Pacage Name
+global.author = '😎Anonymous creator❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_SARA.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://github.com/wuaeyt' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
